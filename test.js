dbd1de5132cf59a9e1121ee75bbc4dee2d5b3df3
edhemphill <edhemphill@wigwag.com>
2016-02-12 19:02:03 -0600

    update

--- test.js
+++ test.js
@@ -4,3 +4,4 @@ console.dir(errs);
 
 console.log("Code was 521: ",errs.HTTP.byStatus[""+521]);
 console.log("Code was 521: ",errs.HTTP.byStatusText["No account tied to user"]);
+console.log("shortCode: ",errs.HTTP.shortCode['NO_ACCOUNT'].status,errs.HTTP.shortCode['NO_ACCOUNT'].statusText);
