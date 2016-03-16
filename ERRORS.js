9958213a7992d1a9d56256c0eff47fe152224ea3
Walt Giroir <walt@giroir.com>
2016-03-16 14:00:09 -0500

    Added relay-already-bound error

--- ERRORS.js
+++ ERRORS.js
@@ -12,8 +12,12 @@ var http_errors = [
 		status: _base + 2,
 		statusText: "No DeviceJS API key tied to account", // Account created / email verified, but no Relay ever paired (so no account)
 		shortCode: "NO_API_KEY"
+	},
+	{
+		status: _base + 3,
+		statusText: "This device is already bound to an account", // Relay Already bound to an account
+		shortCode: "ALREADY_BOUND"
 	}
-
 	/**
 	 * List all further errors here.
 	 *
